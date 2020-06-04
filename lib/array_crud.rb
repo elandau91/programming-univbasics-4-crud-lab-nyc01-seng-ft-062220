ff3f88061dd4ddeca711a603c03742661ac744cc
Eli Landau <eli.b.landau@gmail.com>
2020-06-04 18:00:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  liverpool = ["salah", "mane"]
+  liverpool << "klopp"
 end
 
 def add_element_to_start_of_array(array, element)
